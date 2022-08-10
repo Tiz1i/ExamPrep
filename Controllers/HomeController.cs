6ccff9f0bc9483372de630fe451d2b9f736e641a
Tiz1i <tizianamalko@gmail.com>
2022-08-10 13:33:50 +0200

    ok

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExamPrep.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExamPrep.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
+     
+    // here we can "inject" our context service into the constructor
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+         _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        if(HttpContext.Session.GetInt32("userId") == null){
+            return RedirectToAction("Login");
+            }
+            
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet("Register")]
+    public IActionResult Register(){
+        if(HttpContext.Session.GetInt32("userId") == null){
+          
+              return View();
+            }
+            
+        return RedirectToAction("Index");
+
+    }
+
+    [HttpPost("Register")]
+    public IActionResult Register(User user)
+    {
+        // Check initial ModelState
+        if (ModelState.IsValid)
+        {
+            // If a User exists with provided email
+            if (_context.Users.Any(u => u.Email == user.Email))
+            {
+                // Manually add a ModelState error to the Email field, with provided
+                // error message
+                ModelState.AddModelError("Email", "Email already in use!");
+               
+                return View();
+                // You may consider returning to the View at this point
+            }
+              PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            user.Password = Hasher.HashPassword(user, user.Password);
+             _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        return View();
+    }
+    [HttpGet("Login")]
+    public IActionResult Login()
+    {
+        if (HttpContext.Session.GetInt32("userId") == null)
+        {
+
+            return View();
+        }
+
+        return RedirectToAction("Index");
+    }
+    [HttpPost("Login")]
+    public IActionResult LoginSubmit(LoginUser userSubmission)
+    {
+        if (ModelState.IsValid)
+        {
+            // If initial ModelState is valid, query for a user with provided email
+            var userInDb = _context.Users.FirstOrDefault(u => u.Email == userSubmission.Email);
+            // If no user exists with provided email
+            if (userInDb == null)
+            {
+                // Add an error to ModelState and return to View!
+                ModelState.AddModelError("Email", "Invalid Email/Password");
+                return View("Login");
+            }
+
+            // Initialize hasher object
+            var hasher = new PasswordHasher<LoginUser>();
+
+            // verify provided password against hash stored in db
+            var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.Password);
+
+            // result can be compared to 0 for failure
+            if (result == 0)
+            {
+                ModelState.AddModelError("Password", "Invalid Password");
+                return View("Login");
+                // handle failure (this should be similar to how "existing email" is handled)
+            }
+            HttpContext.Session.SetInt32("userId", userInDb.UserId );
+
+            return RedirectToAction("Index");
+        }
+        return View("Login");
+    }
+
+    [HttpGet("logout")]
+    public IActionResult Logout(){
+
+        HttpContext.Session.Clear();
+        return RedirectToAction("login");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
